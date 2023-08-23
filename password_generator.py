d191c96b476272a4c964f2de9b184540f2de1370
Jordan <jordandansi05@gmail.com>
2023-08-23 08:26:59 +0100

    Jordan project 2

--- password_generator.py
+++ password_generator.py
@@ -10,30 +10,49 @@ purpose = input("Enter the purpose: ")
 length = int(input("Input length: "))
 
 
-def generate_password(length):
+def generate_password(Length: int):
+    """
+    Generates strong password with a specific length
+    :param Length: Integer value given by the user
+    :return: password
+    """
     caractere = string.ascii_letters + string.digits + string.punctuation
-    passwords = ''.join(random.choice(caractere) for _ in range(length))
+    passwords = ''.join(random.choice(caractere) for _ in range(Length))
     print(f"password: {passwords}")
     return passwords
 
 
-def Time():
+def timez():
+    """
+    Gives exact time of creation of password in format HH:MM:SS
+    :return: current time
+    """
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     # print(f"Current Time = {current_time}")
     return current_time
 
-def Date():
+
+def dates():
+    """
+    Gives date on which password is being generated in format DD/MM/YYYY
+    :return: current date
+    """
     import datetime
     current_date = datetime.date.today().strftime("%d/%m/%Y")
     # print(f"Current Date = {current_date}")
     return current_date
+
+
 def add():
-    db.inserts(name, purpose, psw, length, Date(), Time())
+    """
+    Connected to a database, it adds details into psw database
+    :return: none
+    """
+    db.inserts(name, purpose, psw, length, dates(), timez())
 
 
-Time()
-Date()
+# timez()
+# dates()
 psw = generate_password(length)
 add()
-
